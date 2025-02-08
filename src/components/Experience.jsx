21d98a770ecfb61d29da4836c8cb032241ce3980
joel-2112 <eyueljoel21@gmail.com>
2025-02-08 01:47:02 -0800

    feat: add motion utility functions for animations

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,7 @@ import "react-vertical-timeline-component/style.min.css";
 import { styles } from "../styles";
 import { experiences } from "../constants";
 import SectionWrapper from "../hoc/SectionWrapper";
-import { textVariant } from "../utils/motion";
+import  textVariant  from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => {
   return (
